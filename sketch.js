edd86ab9302c272bc8507788ea2143a8f8430026
Rob Duarte <me@robduarte.com>
2023-10-24 14:38:05 -0400

    a working example

--- sketch.js
+++ sketch.js
@@ -1,64 +1,68 @@
-// a simple game where you click to drop a ball and 
-// try to get as many balls as possible on the spinning shape
-// move the mouse up and down to change the rotation speed of the shape
-// use the right and left keys to make the shape larger or smaller
+// a demo showing how to allow drag and drop a sprite
+// and how to know if the sprite was dragged to the right place
 
-let spinningShape;
-let highScore = 0;
+let backgroundImg;
+let jacketImg, dressImg;
+let jacket, dress;
+let dollCenter;
+let whatSheWearing;
+
+function preload() {
+    jacketImg = loadImage("assets/jacket.png");
+    dressImg = loadImage("assets/dress.png");
+    backgroundImg = loadImage("assets/original.jpg");
+}
 
 function setup() {
-    let canvas = new Canvas("fullscreen");
+    new Canvas(1024,727);
+    rectMode(CENTER);
+    allSprites.rotationLock = true;
 
-    world.gravity.y = 10;
+    dollCenter = createVector(133,420);
+    
+    jacket = new Sprite();
+    jacket.img = jacketImg;
+    jacket.position = createVector(900, 500);
+    jacket.drag = 10;
 
-    // this sprite can be created on a single line, but it's easier to read this way:
-	spinningShape = new Sprite();
-	spinningShape.width = canvas.width/5;
-	spinningShape.height = spinningShape.width;
-    spinningShape.collider = "kinematic";
-
-    textFont("Courier", 24);
+    dress = new Sprite();
+    dress.img = dressImg;
+    dress.position = createVector(450, 570);
+    dress.drag = 10;
 }
 
 function draw() {
+    background(backgroundImg);
 
-    // try the game without this line :)
-    clear();
+    if (dress.mouse.dragging()) {
+		dress.moveTowards(
+			mouse.x + dress.mouse.x,
+			mouse.y + dress.mouse.y,
+			1 // a lower number will track the mouse slower
+		);
+	}
 
-    // the map function translates a value from one range to another
-    // https://p5js.org/reference/#/p5/map
-    spinningShape.rotationSpeed = map(mouse.y, 0, canvas.height, -10, 10);
+    if (jacket.mouse.dragging()) {
+		jacket.moveTowards(
+			mouse.x + jacket.mouse.x,
+			mouse.y + jacket.mouse.y,
+			1 // a lower number will track the mouse slower
+		);
+	}
 
-    // create a ball when the mouse is clicked
-    // https://p5play.org/learn/input_devices.html
-    if (mouse.presses()) {
-        let ball = new Sprite(mouse.x, -20, 20);
-        // make the ball resist rolling when it touches the spinning shape
-        // https://p5play.org/learn/sprite.html?page=9
-        ball.rotationDrag = 10;
+    if (dist(dress.x,dress.y,dollCenter.x,dollCenter.y) < 20) {
+        dress.position = dollCenter;
+        whatSheWearing = "dress";
+    } else { 
+        whatSheWearing = "nothing";
     }
 
-    // make the spinning shape larger or smaller using the keyboard
-    // note the difference between presses (above) and pressing (here)
-    if (kb.pressing('right')) {
-        spinningShape.width += 10;
-    } else if (kb.pressing('left')) {
-        spinningShape.width -= 10;
+    if (dist(jacket.x,jacket.y,dollCenter.x,dollCenter.y) < 20) {
+        jacket.position = createVector(dollCenter.x-10, dollCenter.y-10);
+        whatSheWearing = "jacket";
+    } else { 
+        whatSheWearing = "nothing";
     }
 
-    // loop through the allSprites array and see how many are above the center of the screen
-    let currentScore = 0;
-    for (let sprite of allSprites) {
-        if (sprite.y < canvas.height/2) {
-            currentScore++;
-            if (currentScore > highScore) {
-                highScore = currentScore;
-            }
-        }
-    }
-
-    // display the score (minus 1 so it doesn't count the spinning shape)
-    text("BALLS: " + (currentScore-1), 40, 60);
-    text("HIGH:  " + (highScore-1), 40, 86);
-
+    text(whatSheWearing, 100, 100)
 }
